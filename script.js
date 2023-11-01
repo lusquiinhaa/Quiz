d06c1343a0ec29c91a139e30622e53cfce9c25da
Lucas Lima <101532817+lusquiinhaa@users.noreply.github.com>
2023-11-01 09:26:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,133 +11,130 @@ const questions = [
     {
         question: "Qual é a principal função da logística?",
         choices: shuffleArray(["Gerenciar estoques", "Cozinhar comida", "Pintar quadros"]),
-        correctAnswer: 0
+        correctAnswer: "Gerenciar estoques"
     },
     {
         question: "O que é um KPI de logística?",
         choices: shuffleArray(["Key Performance Indicator", "Keep People Informed", "Kilograms per Item"]),
-        correctAnswer: 0
+        correctAnswer: "Key Performance Indicator"
     },
     {
         question: "O que é o JIT (Just in Time) na logística?",
         choices: shuffleArray(["Entregar produtos na hora certa", "Agendar reuniões", "Preparar comida no momento"]),
-        correctAnswer: 0
+        correctAnswer: "Entregar produtos na hora certa"
     },
     {
         question: "O que significa LTL na logística?",
         choices: shuffleArray(["Less Than Truckload", "Large Transportation Load", "Long Travel Line"]),
-        correctAnswer: 0
+        correctAnswer: "Less Than Truckload"
     },
     {
         question: "O que é um armazém de cross-docking?",
         choices: shuffleArray(["Um local de transferência rápida de produtos", "Um museu de arte contemporânea", "Uma escola de mergulho"]),
-        correctAnswer: 0
+        correctAnswer: "Um local de transferência rápida de produtos"
     },
     {
         question: "Qual é o objetivo do roteiramento na logística?",
         choices: shuffleArray(["Encontrar a melhor rota para entrega", "Aprender a tocar guitarra", "Fazer esculturas de areia na praia"]),
-        correctAnswer: 0
+        correctAnswer: "Encontrar a melhor rota para entrega"
     },
     {
         question: "O que é a última milha na logística?",
         choices: shuffleArray(["A entrega final ao cliente", "A última página de um livro", "A última aula de uma palestra"]),
-        correctAnswer: 0
+        correctAnswer: "A entrega final ao cliente"
     },
     {
         question: "O que é um pallet na logística?",
         choices: shuffleArray(["Uma plataforma de carga", "Um animal exótico", "Um tipo de sobremesa"]),
-        correctAnswer: 0
+        correctAnswer: "Uma plataforma de carga"
     },
     {
         question: "O que é um WMS na logística?",
         choices: shuffleArray(["Warehouse Management System", "Web Marketing Strategy", "Wireless Mobile Service"]),
-        correctAnswer: 0
+        correctAnswer: "Warehouse Management System"
     },
     {
         question: "Qual é o objetivo do rastreamento de carga na logística?",
         choices: shuffleArray(["Monitorar a localização dos produtos", "Rastrear animais selvagens", "Registrar dados climáticos"]),
-        correctAnswer: 0
+        correctAnswer: "Monitorar a localização dos produtos"
     },
     {
         question: "O que é o lead time na logística?",
         choices: shuffleArray(["O tempo entre o pedido e a entrega", "O tempo que um carro leva para acelerar", "O tempo que uma pessoa leva para responder a um e-mail"]),
-        correctAnswer: 0
+        correctAnswer: "O tempo entre o pedido e a entrega"
     },
     {
         question: "O que é um incoterms na logística?",
         choices: shuffleArray(["Termos de Comércio Internacional", "Termos de Contrato Incorretos", "Termos de Incêndio e Emergência"]),
-        correctAnswer: 0
+        correctAnswer: "Termos de Comércio Internacional"
     },
     {
         question: "O que é um contêiner ISO na logística?",
         choices: shuffleArray(["Padrão de contêiner intermodal", "Uma organização internacional", "Um formato de arquivo de imagem"]),
-        correctAnswer: 0
+        correctAnswer: "Padrão de contêiner intermodal"
     },
     {
         question: "O que é um caminhão cegonha na logística?",
         choices: shuffleArray(["Um veículo para transportar carros", "Um caminhão de sorvetes", "Um tipo de avião militar"]),
-        correctAnswer: 0
+        correctAnswer: "Um veículo para transportar carros"
     },
     {
         question: "O que é a embalagem primária na logística?",
         choices: shuffleArray(["A embalagem do produto em si", "Uma embalagem secundária", "A embalagem do presente de aniversário"]),
-        correctAnswer: 0
+        correctAnswer: "A embalagem do produto em si"
     },
     {
         question: "O que é um TMS na logística?",
         choices: shuffleArray(["Transportation Management System", "Time Management Strategy", "Trade Marketing Service"]),
-        correctAnswer: 0
+        correctAnswer: "Transportation Management System"
     },
     {
         question: "O que é um RFID na logística?",
         choices: shuffleArray(["Identificação por Radiofrequência", "Rápido e Fácil de Usar", "Registro de Função de Instrução Direta"]),
-        correctAnswer: 0
+        correctAnswer: "Identificação por Radiofrequência"
     },
     {
         question: "O que é um SLA na logística?",
         choices: shuffleArray(["Acordo de Nível de Serviço", "Sala de Arte", "Símbolo de Linguagem Alternativa"]),
-        correctAnswer: 0
+        correctAnswer: "Acordo de Nível de Serviço"
     },
     {
         question: "O que é um CRM na logística?",
         choices: shuffleArray(["Gestão de Relacionamento com o Cliente", "Clube de Rock Moderno", "Controle de Reservas de Mesa"]),
-        correctAnswer: 0
+        correctAnswer: "Gestão de Relacionamento com o Cliente"
     },
     {
         question: "O que é a intermodalidade na logística?",
         choices: shuffleArray(["Uso de diferentes modos de transporte", "Interação social", "Uso de um único modo de transporte"]),
-        correctAnswer: 0
+        correctAnswer: "Uso de diferentes modos de transporte"
     },
     // Perguntas adicionais sobre transporte, armazenamento, placas de risco e termos técnicos
     {
         question: "O que é um contentor intermodal?",
         choices: shuffleArray(["Um recipiente de transporte de carga", "Uma estação de rádio", "Um instrumento musical"]),
-        correctAnswer: 0
+        correctAnswer: "Um recipiente de transporte de carga"
     },
     {
         question: "O que significa a sigla OEA no contexto da logística?",
         choices: shuffleArray(["Operador Econômico Autorizado", "Ordem de Entrega Atrasada", "Organização de Empreendedores Aventureiros"]),
-        correctAnswer: 0
+        correctAnswer: "Operador Econômico Autorizado"
     },
     {
         question: "O que são placas de risco na logística?",
         choices: shuffleArray(["Símbolos que indicam perigos de substâncias", "Sinais de trânsito", "Tabelas de classificação de alimentos"]),
-        correctAnswer: 0
+        correctAnswer: "Símbolos que indicam perigos de substâncias"
     },
     {
         question: "O que é um WMS na logística?",
         choices: shuffleArray(["Warehouse Management System", "Web Marketing Strategy", "Wireless Mobile Service"]),
-        correctAnswer: 0
+        correctAnswer: "Warehouse Management System"
     },
     {
         question: "O que significa o termo 'cross-docking'?",
         choices: shuffleArray(["Processo de transferência rápida de mercadorias", "Prática de cruzar estradas", "Esporte radical"]),
-        correctAnswer: 0
+        correctAnswer: "Processo de transferência rápida de mercadorias"
     },
 ];
-
-// Resto do código permanece o mesmo
-
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -149,10 +146,12 @@ function displayQuestion() {
     const choicesList = document.getElementById("choices");
     choicesList.innerHTML = "";
 
-    currentQuestion.choices.forEach((choice, index) => {
+    const shuffledChoices = currentQuestion.choices;
+
+    shuffledChoices.forEach((choice, index) => {
         const choiceButton = document.createElement("button");
         choiceButton.textContent = choice;
-        choiceButton.onclick = () => checkAnswer(index);
+        choiceButton.onclick = () => checkAnswer(choice);
         choicesList.appendChild(document.createElement("li").appendChild(choiceButton));
     });
 
@@ -161,13 +160,13 @@ function displayQuestion() {
     document.getElementById("prev-button").disabled = currentQuestionIndex === 0;
 }
 
-function checkAnswer(selectedIndex) {
+function checkAnswer(selectedChoice) {
     const currentQuestion = questions[currentQuestionIndex];
-    if (selectedIndex === currentQuestion.correctAnswer) {
+    if (selectedChoice === currentQuestion.correctAnswer) {
         score++;
         document.getElementById("result").textContent = "Correto!";
     } else {
-        document.getElementById("result").textContent = "Incorreto. A resposta correta é: " + currentQuestion.choices[currentQuestion.correctAnswer];
+        document.getElementById("result").textContent = "Incorreto. A resposta correta é: " + currentQuestion.correctAnswer;
     }
 
     document.getElementById("next-button").disabled = false;
